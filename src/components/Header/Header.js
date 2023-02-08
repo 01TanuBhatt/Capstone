9b7d402cc34ff9b8ac0a7cb977e8609d4e0424e5
Tanu <01tanubhatt@gmail.com>
2023-02-08 16:37:40 +0530

    design of calculator

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -2,8 +2,18 @@ import React from 'react';
 import './Header.css'
 export default function Header() {
   return (
-    <div className="header">
-      <h1>Header</h1>
+    <div className="header custom-scroll">
+      <div className="header_history">
+      <p>10-9</p>
+      <p>2345-9887</p>
+      <p>34*78</p>
+      <p>12345/78</p>
+      </div>
+      <br></br>
+      <div className="header_expression custom-scroll">
+        <p>10+50+89</p>
+      </div>
+      <p className="header_result">149</p>
     </div>
   )
 }
