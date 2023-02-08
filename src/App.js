92879613102393aba08556b69edb871c80c47c67
Tanu <01tanubhatt@gmail.com>
2023-02-08 15:59:55 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
+import React,{useState} from 'react'
 import './App.css';
-
+import Header from './components/Header/Header';
+import KeyPad from './components/KeyPad/KeyPad';
 function App() {
+
+  const[isDarkMode,setIsDarkMode] = useState(false)
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app" data-theme={isDarkMode?"dark":""}>
+      <div className="app_calculator">
+        <div className="app_calculator_navbar">
+          <div
+            className="app_calculator_navbar_toggle"
+            onClick={() => setIsDarkMode(!isDarkMode)}
+          >
+            <div
+              className={`app_calculator_navbar_toggle_circle ${
+                isDarkMode ? "app_calculator_navbar_toggle_circle_active" : ""
+              }`}
+            />
+          </div>
+          <img src={isDarkMode ? "https://raw.githubusercontent.com/handyDev2/react-calculator/master/src/assets/moon.png" : "https://github.com/handyDev2/react-calculator/blob/master/src/assets/sun.png?raw=true"} alt="mode" />
+        </div>
+     <Header/>
+     <KeyPad/>
+      
+    </div>
     </div>
   );
 }
---
+++ src/components/KeyPad/KeyPad.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import './KeyPad.css'
+export default function KeyPad() {
+  return (
+    <div className="keypad">
+      <h1>KeyPad</h1>
+    </div>
+  )
+}
